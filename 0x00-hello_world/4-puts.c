b28feb57a3b4f367e0b3fd6ff80aa971a0dc91c6
Minno007 <Menna.soft333@gmail.com>
2023-04-24 18:20:46 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
-int main(void){
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+
+/* puts */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
