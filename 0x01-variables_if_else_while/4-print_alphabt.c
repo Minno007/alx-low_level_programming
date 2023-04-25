c194233565713f5a5d124a27d590ea19b778be28
Minno007 <Menna.soft333@gmail.com>
2023-04-24 21:14:47 -0700

    4task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: "l except q e"
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		if (letter != 'q' && letter != 'e')
+			putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
