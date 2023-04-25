69224fab724b1a08b88ecf7b05a71e34549c0465
Minno007 <Menna.soft333@gmail.com>
2023-04-24 18:34:48 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
-/* puts */
+/**  
+ * main - Entry point
+ *  
+ * Decription: A C program that prints with put function
+ *
+ * Return: Alawys 0 (Seccsess)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
